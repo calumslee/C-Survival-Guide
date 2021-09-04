7cec73697f8f64c7e9a742203f863242a7d76b09
Calum Slee <calumslee@hotmail.co.nz>
2021-09-05 11:28:33 +1200

    Singletons completed

---
+++ Assets/Scripts/Singletons/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    //Create private instance
+    //Create public property to reference instance
+    //Null check
+    //Return private instance
+
+    //Assign instance on awake
+
+    private static SpawnManager _instance;
+    public static SpawnManager Instance
+    {
+        get
+        {
+            if (_instance == null) Debug.LogError("Spawn Manager is NULL!");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void StartSpawning()
+    {
+        Debug.Log("Starting spawning");
+    }
+}
